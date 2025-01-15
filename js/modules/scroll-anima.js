442f5c1f9cbcd674583d496319b87fa5bf22ef58
willianforgiarini <forgwill0715@outlook.com>
2025-01-15 17:00:50 -0300

    refactor: :recycle: refatorando algumas funções

---
+++ js/modules/scroll-anima.js
@@ -0,0 +1,46 @@
+export default class ScrollAnima {
+  constructor(sections) {
+    this.sections = document.querySelectorAll(sections);
+    this.windowMetade = window.innerHeight * 0.6;
+    this.checkDistance = this.checkDistance.bind(this);
+  }
+
+  // pega a distância de cada item em relação
+  // ao topo do site
+  getDistance() {
+    this.distance = [...this.sections].map(section => {
+      const offset = section.offsetTop;
+      return {
+        element: section,
+        offset: Math.floor(offset) - this.windowMetade,
+      };
+    });
+    console.log(this.distance);
+  }
+
+  // vetifica a distância em cada objeto
+  // em relação ao scroll do site
+  checkDistance() {
+    this.distance.forEach(section => {
+      if (window.scrollY > section.offset) {
+        section.element.classList.add('ativo');
+      } else if (section.element.classList.contains('ativo')) {
+        section.element.classList.remove('ativo')
+      }
+    })
+  }
+    
+  init() {
+    if (this.sections.length) {
+      this.getDistance();
+      this.checkDistance(); // para abrir o site e ja carregar uma section
+      window.addEventListener("scroll", this.checkDistance);
+    }
+    return this;
+  }
+
+  // remove o evento de scroll
+  stop() {
+    window.removeEventListener("scroll", this.checkDistance)
+  }
+}
--- js/script.js
+++ js/script.js
@@ -3,14 +3,13 @@ import ScrollSuave from "./modules/scroll-suave.js";
 import Accordion from "./modules/accordion.js";
 import TabNav from "./modules/tab-nav.js";
 import Modal from "./modules/modal.js";
-import initTooltip from "./modules/tooltip.js";
+import Tooltip from "./modules/tooltip.js";
 import initDropdownMenu from "./modules/dropdown-menu.js";
 import initMenuMobile from "./modules/menu-mobile.js";
-// import initAnimaNumeros from "./modules/anima-numeros.js";
 import initFuncionamento from "./modules/funcionamento.js";
-import initFetchAnimais from "./modules/fetch-animais.js";
-import initFetchBitcoin from "./modules/fetch.bitcoin.js";
-import initAnimacaoScroll from "./modules/scroll-animacao.js";
+import FetchAnimais from "./modules/fetch-animais.js";
+import fetchBitcoin from "./modules/fetch.bitcoin.js";
+import ScrollAnima from "./modules/scroll-anima.js";
 
 // o ideal é exportar somente uma coisa, para que o modulo seja especifico em fazer somente uma coisa
 // as funções dos arquivos são carregadas em forma assincrona, porém são executadas na ordem que foi colocada
@@ -27,11 +26,14 @@ tabNav.init()
 const modal = new Modal('[data-modal="abrir"]', '[data-modal="fechar"]', '[data-modal="container"]');
 modal.init();
 
-initTooltip();
+const toolTip = new Tooltip('[data-tooltip]');
+toolTip.init();
+
+const scollAnima = new ScrollAnima('[data-anime="scroll"]');
+scollAnima.init();
+
 initDropdownMenu();
 initMenuMobile();
-// initAnimaNumeros();
 initFuncionamento();
-initFetchAnimais();
-initFetchBitcoin();
-initAnimacaoScroll();
+FetchAnimais('../animaisapi.json', '.numeros-grid');
+fetchBitcoin('https://www.blockchain.com/pt/ticker', '.btc-preco');
